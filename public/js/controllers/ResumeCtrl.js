689b7be9ed91f34746fb2c1a74ad557a285a350d
Jeremy Greer <jeremygreer@reergymerej.com>
2014-05-05 12:47:56 -0400

    generate skill columns dynamically

--- public/js/controllers/ResumeCtrl.js
+++ public/js/controllers/ResumeCtrl.js
@@ -1,7 +1,26 @@
 (function () {
     'use strict';
     angular.module('jg').controller('ResumeCtrl', ['$scope', '$http', function ($scope, $http) {
+
         $scope.resume = [];
+        $scope.skillGroups = [];
+        $scope.skillGroupCount = 4;
+
+        var divideSkills = function () {
+            var i,
+                groups = $scope.skillGroups,
+                skills = $scope.resume.skills;
+
+            // create groups
+            for (i = 0; i < $scope.skillGroupCount; i = i + 1) {
+                groups.push([]);
+            }
+
+            // dole out the skills
+            for (i = 0; i < skills.length; i = i + 1) {
+                groups[i % $scope.skillGroupCount].push(skills[i]);
+            }
+        };
 
         $http({
             method: 'GET',
@@ -9,9 +28,13 @@
         }).
         success(function (data) {
             $scope.resume = data;
+
+            divideSkills();
         })
         .error(function (data) {
             console.error('whoops');
         });
+
+
     }]);
 }());
